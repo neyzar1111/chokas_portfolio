e565e259982a947026122d72251ffd26989aca94
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-09-21 18:25:19 +0200

    Add styles icons

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,29 +5,54 @@ function AboutMe() {
     const useDiv2 = useRef();
     const [scroll, setScroll] = useState(0);
 
-    window.onscroll = () =>{
-        setScroll(window.scrollY - 800);
-        console.log(scroll)
-        useDiv.current.style.left = `${scroll}px` ;
-    }
     return (
         <div id="about_me">
             <div className="aboutmeContentWrap">
-                <div>
-                    <h2>Hello, my name is Aichurok and I am junior web developer!</h2>
-                    I am passionate about building scalable software, creating effective solutions, and learning every day to grow professionally in the IT field.
-                </div>
-            </div>
-            <div className="scrollSection">
-                <div ref={useDiv} className="line1">
-                    <h1>Line1 Line1 Lin1</h1>
-                </div>
-
-                <div ref={useDiv2} className="line2">
-                    <h1> Line 2 Line 2 Line2 Line2</h1>
+                <div className= "imgWrap"> <div className="chokaImg"></div></div>
+                <div className="text-intro">
+                   <h2> Hello, my name is Aichurok and I am a junior web developer!</h2>
+                    <p>
+                        I am passionate about building scalable software, creating effective solutions, and learning every day to grow professionally in the IT field.
+                    </p>
                 </div>
 
             </div>
+            <section className="skills">
+                <div className="rows animate__animated animate__fadeInUp">
+                    <h3>Design</h3>
+                    <div className="icon-wrap">
+                        <img  className="" src="https://img.icons8.com/ios/30/ffffff/sass-avatar.png"/>
+                        <img  className="" src="https://img.icons8.com/windows/30/ffffff/bootstrap.png"/>
+                        <img  className="" src="https://img.icons8.com/ios/30/ffffff/html.png"/>
+                    </div>
+                </div>
+                <div className="rows animate__animated animate__fadeInUp">
+                    <h3>Code</h3>
+                    <div className="icon-wrap">
+                        <img  className="" src="https://img.icons8.com/ios-filled/30/ffffff/php.png"/>
+                        <img  className="" src="https://img.icons8.com/ios-filled/30/ffffff/js.png"/>
+                        <img className="" src="https://img.icons8.com/ios/30/ffffff/laravel.png"/>
+                        <img className="" src="https://img.icons8.com/dotty/30/ffffff/react.png"/>
+                    </div>
+                </div>
+                <div className="rows">
+                    <h3>Tools</h3>
+                    <div className="icon-wrap animate__animated animate__fadeInUp">
+                        <img className="" src="https://img.icons8.com/ios-filled/30/ffffff/git.png"/>
+                        <img className="" src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png"/>
+                        <img className="" src="https://img.icons8.com/windows/30/ffffff/nodejs.png"/>
+                    </div>
+                </div>
+            </section>
+            <section className="quoteSection">
+                <div className="blockquoteWrap">
+                    <blockquote>
+                        <q>All our dreams can come true, if we have the courage to pursue them. </q>
+                        <cite> – Walt Disney</cite>
+                    </blockquote>
+                </div>
+
+            </section>
         </div>
     );
 }
