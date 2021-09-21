7a0274463ab5306b9c29045f6a9c88e130123815
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-09-21 09:52:58 +0200

    Add app js with routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Contacts from "./components/Contacts";
+import AboutMe from "./components/AboutMe";
+import Header from "./components/Header";
+import My_projects from "./components/My_projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="my_portfolio">
+          <Header />
+          <switch>
+            <Route exact path="/" component={AboutMe}/>
+              <Route exact path="/My_projects" component={My_projects}/>
+              <Route exact path="/Contacts" component={Contacts}/>
+          </switch>
+        </div>
+        <Footer />
+      </Router>
   );
 }
+
 
 export default App;
