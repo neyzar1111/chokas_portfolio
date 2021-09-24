f8edb61e573448be7551cb73b8d5c08db84fc71d
neyzar1111 <choka95@hotmail.com>
2021-09-24 15:53:11 +0200

    Add contact form

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,6 +2,29 @@
 function Contacts() {
     return (
         <div id="contacts">
+            <div className="form_wrapper">
+                <h3>Say Hello!</h3>
+                <form className="form_contact">
+                    <p>
+                        <label>Name</label>
+                        <input type="text" name='name'/>
+                    </p>
+                    <p>
+                        <label>E-mail</label>
+                        <input type="e-mail" name='e-mail'/>
+                    </p>
+                    <p>
+                        <label>Message</label>
+                        <textarea type="message" rows="5" name='message'></textarea>
+                    </p>
+                    <p>
+                        <button >Submit</button>
+                    </p>
+
+
+                </form>
+
+            </div>
         </div>
     );
 }
