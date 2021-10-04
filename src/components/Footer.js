e25f60f673f5ae0730e8edba9ed51a22d3256e89
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-10-04 22:00:52 +0200

    Add buttons to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,18 +18,27 @@ function Footer() {
                     Let's get in touch and talk more about your projects.
                 </p>
                 <div className='footer-icon_Container' >
-                    <a href="mailto:choka95@hotmail.com" target="_blank" >
-                        <img className= "footer-icon icon-skills" src="/assets/email2.svg"/>
-                    </a>
 
+                    <a href="mailto:choka95@hotmail.com" target="_blank" >
+                       <button className="intro-button">
+                           Send Me Email
+                           <img className= "icon" src="/assets/email2.svg"/>
+                       </button>
+                    </a>
                     <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin">
-                        <img className= "footer-icon icon-skills" src="/assets/linkedin2.svg"/>
+                        <button className="intro-button">
+                            Watch My LinkedIn
+                            <img className= "icon" src="/assets/linkedin2.svg"/>
+                        </button>
 
                     </a>
                     <a href="https://github.com/neyzar1111">
-                        <img className= "footer-icon icon-skills" src="/assets/github.svg"/>
-
+                        <button className="intro-button">
+                            Watch My LinkedIn
+                            <img className= "icon" src="/assets/github.svg"/>
+                        </button>
                     </a>
+
                 </div>
             </div>
 
